a187fbbb5d682f12338a119f49c77ec46d6efadf
Anupama Gadde <anupamagadde@yahoo.com>
2019-12-11 10:57:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,8 @@ def badge_maker (name)
   return "Hello, my name is #{name}."
 end
 def batch_badge_creator(array)
+  badge_array  = []
   array.each do |name|
-    
+    badge_array << "Hello, my name is #{name}."
+  end
 end
